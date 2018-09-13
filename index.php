20b98a11746615ca2d2a00e21934113015a814b4
root <root@ip-172-31-35-215.ec2.internal>
2018-09-13 04:44:27 +0000

    Added the beginning files for a blank website.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require('blocks/header.php'); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
+
+<?php require('blocks/footer.php'); ?>
