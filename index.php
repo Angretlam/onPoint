a02e2709c198bbda11f5521ba56966128c22dd8b
root <root@ip-172-31-35-215.ec2.internal>
2018-09-25 03:03:16 +0000

    MEh

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <?php require('blocks/header.php'); ?>
 
-    <div class="jumbotron">
-      <h1 class="display-4">On Point Abstracting</h1>
-      <p class="lead">“OnPoint to deliver quality service”</p>
+    <div class="jumbotron" style="background-image: url('/blocks/Background_Tiles.png');">
+      <h1 class="display" style="color: white;">On Point Abstracting</h1>
+      <h4 style="color: white;">“OnPoint to deliver quality service”</h4>
     </div>
 
   <div class="container">
     <div class="row">
-      <h4>INTEGRITY. EXPERIENCE. INNOVATION.</h4>
-      
-      <p>OnPoint Abstracting provides full-service abstracting services to the entire State of Minnesota.  We pride ourselves in making you look good by providing professional, courteous and fast service.  Our office staff is available from 6:00 am to 6:00 pm.  We ensure the fastest turnaround time</p>
+      <div class="col-sm-8">
+        <h4>INTEGRITY. EXPERIENCE. INNOVATION.</h4>
+        
+        <p>OnPoint Abstracting provides full-service abstracting services to the entire State of Minnesota.  We pride ourselves in making you look good by providing professional, courteous and fast service.  Our office staff is available from 6:00 am to 6:00 pm.  We ensure the fastest turnaround time</p>
 
-      <p>OnPoint Abstracting values the input of our clients above all else. Our professional and experienced staff is here to answer any questions you may have about our company or our services. We strive to offer nothing less than quality performance and reliable service.</p>
+        <p>OnPoint Abstracting values the input of our clients above all else. Our professional and experienced staff is here to answer any questions you may have about our company or our services. We strive to offer nothing less than quality performance and reliable service.</p>
 
-      <p>We are dedicated to the assurance that each transaction receives the utmost attention to detail and accuracy. Our goal is to provide each client with courteous, expedient, professional service of the highest caliber.</p>
-    </div>
-
-    <div class="row">
+        <p>We are dedicated to the assurance that each transaction receives the utmost attention to detail and accuracy. Our goal is to provide each client with courteous, expedient, professional service of the highest caliber.</p>
+    
+      <h5>Accuracy.</h5>
+      <p>Our computerized on-site title plant and team of professionals - ranging from attorneys to commercial and residential title services specialists - give you the most up-to-date, thoroughly researched data possible.</p>
+      <h5>Thoroughness.</h5>
+      <p>It’s that one little detail that can come back to haunt you. That's why we don’t believe in having one person do it all - we believe in having three people specialize in each aspect of the project. A Searcher/Examiner, a Processor and a Proofreader work together to see the product is complete, accurate and on time, every time.</p>
+      <h5>Flexibility.</h5>
+      <p>With the modern technology that we make sure to keep up with on a daily basis, we have 24/7/365 access to all necessary data to provide you with up to date accurate and thorough searches! Online processing gives you instant access to us. And the availability of a diverse staff of experienced professionals means we can be there when you need us to be.</p>
+      <h5>Personal Service.</h5>
+      <p>Technology is nothing without the people to back it up. You won’t find another abstracting company with as many diverse offerings or professionals to meet your needs. You won’t find another that can match our level of service, ensuring timely completion of all searches, reports and exams, every time.</p>
+      </div>
       <div class="col-sm-4">
         <div class="card">
           <img class="card-img-top" src="/blocks/Title.png" alt="Card image cap">
@@ -26,8 +34,7 @@
             <a href="/services.php" class="btn btn-primary">Title Services</a>
           </div>
         </div>
-      </div>
-      <div class="col-sm-4">
+        <br />
         <div class="card">
           <img class="card-img-top" src="/blocks/Search.png" alt="Card image cap">
           <div class="card-body">
@@ -36,8 +43,7 @@
             <a href="/services.php" class="btn btn-primary">Search Services</a>
           </div>
         </div>
-      </div>
-      <div class="col-sm-4">
+        <br />
         <div class="card">
           <img class="card-img-top" src="/blocks/Report.png" alt="Card image cap">
           <div class="card-body">
@@ -47,21 +53,6 @@
           </div>
         </div>
       </div>
-    </div>
-    
-    <div class="row">
-      <h5>Accuracy.</h5>
-      <p>Our computerized on-site title plant and team of professionals - ranging from attorneys to commercial and residential title services specialists - give you the most up-to-date, thoroughly researched data possible.</p>
-
-      <h5>Thoroughness.</h5>
-      <p>It’s that one little detail that can come back to haunt you. That's why we don’t believe in having one person do it all - we believe in having three people specialize in each aspect of the project. A Searcher/Examiner, a Processor and a Proofreader work together to see the product is complete, accurate and on time, every time.</p>
-
-      <h5>Flexibility.</h5>
-      <p>With the modern technology that we make sure to keep up with on a daily basis, we have 24/7/365 access to all necessary data to provide you with up to date accurate and thorough searches! Online processing gives you instant access to us. And the availability of a diverse staff of experienced professionals means we can be there when you need us to be.</p>
-
-      <h5>Personal Service.</h5>
-      <p>Technology is nothing without the people to back it up. You won’t find another abstracting company with as many diverse offerings or professionals to meet your needs. You won’t find another that can match our level of service, ensuring timely completion of all searches, reports and exams, every time.</p>
-
     </div>
   </div>
 
